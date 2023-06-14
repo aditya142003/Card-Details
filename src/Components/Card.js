c5b7b03666cfce76fdbdfffc0a84a8b7c90fe5f9
aditya142003 <adityabhatnagar142003@gmail.com>
2023-06-14 13:30:58 +0530

    Functionalities Completed

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,42 +1,130 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Style/Card.css";
 import chip from "./Images/chip.png";
 import map from "./Images/map.png";
 import visa from "./Images/visa.png";
 
 function Card(props) {
-  var first = [1, 2, 3, 4];
-  var second = [1, 2, 3, 4];
-  var third = [1, 2, 3, 4];
-  var forth = [1, 2, 3, 4];
-  var arr = [first, second, third, forth];
+  const [first, setfirst] = useState("####");
+  const [second, setsecond] = useState("####");
+  const [third, setthird] = useState("####");
+  const [forth, setforth] = useState("####");
+  const [fullname, setfullname] = useState("FULL NAME");
+  const [cvvs, setcvvs] = useState("####");
+  const [expiryMon, setexpiryMon] = useState("MM");
+  const [expiryYear, setexpiryYear] = useState("YYYY");
+  const star = "****";
+
   document.documentElement.style.setProperty("--dimesnion", props.dimension);
+
+  const cardNumber = JSON.parse(props.cardNumber);
+  const holderName = JSON.parse(props.holderName);
+  const cvv = JSON.parse(props.cvv);
+
+  useEffect(() => {
+    // console.log(data);
+    if (cardNumber.focus == true) {
+      document.getElementById("cardNum").style.border = "white 3px solid";
+    } else {
+      document.getElementById("cardNum").style.border = "white 0px solid";
+    }
+    var num = cardNumber.value.length;
+    var str = cardNumber.value;
+    if (num <= 4) {
+      var temp = str + first.slice(num, 4);
+      setfirst(temp);
+    } else if (num > 4 && num <= 8) {
+      var temp = star.slice(0, num - 4) + second.slice(num - 4, 4);
+      setsecond(temp);
+    } else if (num > 8 && num <= 12) {
+      var temp = star.slice(0, num - 8) + third.slice(num - 8, 4);
+      setthird(temp);
+    } else if (num > 12 && num <= 16) {
+      var temp = str.slice(12, num) + forth.slice(num - 12, 4);
+      setforth(temp);
+    }
+
+    if (holderName.focus == true) {
+      document.getElementById("cardFullName").style.border = "white 3px solid";
+    } else {
+      document.getElementById("cardFullName").style.border = "white 0px solid";
+    }
+    if (holderName.value.length != 0) {
+      setfullname(holderName.value);
+    } else {
+      setfullname("FULL NAME");
+    }
+
+    if (props.dateFocus == true) {
+      document.getElementById("expiryDate").style.border = "white 3px solid";
+    } else {
+      document.getElementById("expiryDate").style.border = "white 0px solid";
+    }
+    if (props.month) {
+      setexpiryMon(props.month + 1);
+      setexpiryYear(props.year);
+    } else {
+      setexpiryMon("MM");
+      setexpiryYear("YYYY");
+    }
+
+    if (cvv.focus == true) {
+      document.getElementById("innerContainer").style.transform =
+        "rotateY(180deg)";
+    } else {
+      document.getElementById("innerContainer").style.transform =
+        "rotateY(0deg)";
+    }
+    var cvvlength = cvv.value.length;
+    if (cvv.value.length != 0) {
+      var temp = cvv.value + cvvs.slice(cvvlength, 4);
+      setcvvs(temp);
+    } else {
+      setcvvs("####");
+    }
+  }, [
+    cardNumber.focus,
+    cardNumber.value,
+    holderName.value,
+    holderName.focus,
+    cvv.focus,
+    cvv.value,
+    props.dateFocus,
+    props.month,
+    props.year,
+  ]);
+
   return (
     <div className="container">
-      <div className="innerConatiner">
+      <div className="innerConatiner" id="innerContainer">
         <div className="front">
           <img src={map} className="map"></img>
           <img src={chip} className="chip"></img>
           <img src={visa} className="visa"></img>
-          <div className="cardNum">
-            {arr.map((e) => {
-              return (
-                <span className="cardNumFChild">
-                  {e.map((t) => {
-                    return <span>{t}&nbsp;</span>;
-                  })}
-                </span>
-              );
-            })}
+          <div className="cardNum" id="cardNum">
+            <span className="cardNumChild">
+              <span>{first}&nbsp;</span>
+              <span>{second}&nbsp;</span>
+              <span>{third}&nbsp;</span>
+              <span>{forth}&nbsp;</span>
+            </span>
           </div>
           <div className="bottom">
             <div className="cardName">
               <div>Card Holder</div>
-              <div>FULL NAME</div>
+              <div id="cardFullName" className="cardFullName">
+                {fullname}
+              </div>
             </div>
             <div className="cardDate">
               <div>Expires</div>
-              <div>MM/YY</div>
+              <div id="expiryDate" className="expiryDate">
+                {expiryMon != "MM"
+                  ? `${
+                      expiryMon / 10 >= 1 ? expiryMon : "0" + expiryMon
+                    }/${expiryYear}`
+                  : "MM/YYYY"}
+              </div>
             </div>
           </div>
         </div>
@@ -44,7 +132,9 @@ function Card(props) {
           <img src={map} className="map"></img>
           <div className="blackStrip"></div>
           <span className="cvvWord">CVV</span>
-          <div className="cvv"></div>
+          <div className="cvv" id="cardcvv">
+            {cvvs}
+          </div>
           <img src={visa} className="visaBack"></img>
         </div>
       </div>
--- src/Components/MainArea.js
+++ src/Components/MainArea.js
@@ -1,11 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Style/MainArea.css";
 import Card from "./Card";
-
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 function MainArea() {
+  const [cardNumber, setCardnumber] = useState({
+    focus: false,
+    value: [],
+  });
+  const [holderName, setholderName] = useState({
+    focus: false,
+    value: "",
+  });
+  const [expiryDate, setExpiryDate] = useState({
+    focus: false,
+    value: new Date(),
+  });
+  const [cvv, setcvv] = useState({
+    focus: false,
+    value: [],
+  });
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
-      <Card dimension="500px" />
+      <div>
+        <Card
+          dimension="200px"
+          cardNumber={JSON.stringify(cardNumber)}
+          holderName={JSON.stringify(holderName)}
+          cvv={JSON.stringify(cvv)}
+          month={expiryDate.value.getMonth()}
+          year={expiryDate.value.getFullYear()}
+          dateFocus={expiryDate.focus}
+        />
+      </div>
+      <div>
+        <form>
+          <div>Card Number</div>
+          <input
+            type="number"
+            onFocus={(e) => {
+              setCardnumber({ ...cardNumber, focus: true });
+            }}
+            onBlur={(e) => {
+              setCardnumber({ ...cardNumber, focus: false });
+            }}
+            onChange={(e) => {
+              setCardnumber({ ...cardNumber, value: e.target.value });
+            }}
+          ></input>
+          <div>Holder Name</div>
+          <input
+            type="text"
+            onFocus={(e) => {
+              setholderName({ ...holderName, focus: true });
+            }}
+            onBlur={(e) => {
+              setholderName({ ...holderName, focus: false });
+            }}
+            onChange={(e) => {
+              setholderName({ ...holderName, value: e.target.value });
+            }}
+            id="holderName"
+          ></input>
+          <div>Expiry Date</div>
+          <button className="example-custom-input" onClick={handleClick}>
+            {(expiryDate.value.getMonth() + 1) / 10 >= 1
+              ? expiryDate.value.getMonth() + 1
+              : "0" + (expiryDate.value.getMonth() + 1)}
+            /{expiryDate.value.getFullYear()}
+          </button>
+          {isOpen && (
+            <DatePicker
+              minDate={new Date()}
+              showMonthYearPicker
+              onFocus={() => {
+                setExpiryDate({ ...expiryDate, focus: true });
+              }}
+              onBlur={() => {
+                setExpiryDate({ ...expiryDate, focus: false });
+              }}
+              onChange={(date) => {
+                setExpiryDate({ ...expiryDate, value: date });
+              }}
+              inline
+            />
+          )}
+
+          <div>CVV</div>
+          <input
+            type="text"
+            onFocus={(e) => {
+              setcvv({ ...cvv, focus: true });
+            }}
+            onBlur={(e) => {
+              setcvv({ ...cvv, focus: false });
+            }}
+            onChange={(e) => {
+              setcvv({ ...cvv, value: e.target.value });
+            }}
+            id="cvv"
+          ></input>
+        </form>
+      </div>
     </div>
   );
 }
